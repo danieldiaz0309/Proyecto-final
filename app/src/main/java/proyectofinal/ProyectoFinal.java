522c63cc9e70a9a1f7e933397d030c05e48e56ed
danieldiaz0309 <daniel.diaz0309@gmail.com>
2026-05-19 21:18:45 -0500

    Vea listos melos todo otra vez

--- app/src/main/java/proyectofinal/ProyectoFinal.java
+++ app/src/main/java/proyectofinal/ProyectoFinal.java
@@ -1,5 +1,4 @@
 package proyectofinal;
-
 import javax.swing.JOptionPane;
 
 public class ProyectoFinal {
@@ -14,15 +13,15 @@ public class ProyectoFinal {
     }
 
     public static boolean hayPedido(int[][] cantidades) {
-        for (int i = 0; i < cantidades.length; i++) {
-            for (int j = 0; j < cantidades[i].length; j++) {
-                if (cantidades[i][j] > 0) {
-                    return true;
-                }
+    for (int i = 0; i < cantidades.length; i++) {
+        for (int j = 0; j < cantidades[i].length; j++) {
+            if (cantidades[i][j] > 0) {
+                return true;
             }
         }
-        return false;
     }
+    return false;
+}
 
     // Función para generar factura
     public static String generarFactura(String[][] nombres, int[][] precios, int[][] cantidades) {
@@ -47,28 +46,25 @@ public class ProyectoFinal {
         return factura;
     }
 
-    // Commit 3: Creación manualamente e ingreso de los datos en las matrices,
-    // selección de tipo de usuario y validación de clave para administrador
     public static void main(String[] args) {
 
-        // MATRICES - Categorías: Hamburguesas, Perros, Perras, Papas, Adicionales,
-        // Bebidas
+        // MATRICES (requisito)
         String[][] nombres = {
-                { "Hamburguesa Sencilla", "Hamburguesa Especial", "Hamburguesa de la Casa" },
-                { "Perro Sencillo", "Perro Especial", "Super Dog" },
-                { "Perra Sencilla", "Perra Especial", "Super Doggy" },
-                { "Papas a la francesa" },
-                { "Queso", "Tocineta", "Ensalada" },
-                { "Gaseosa Personal", "Gaseosa Familiar", "Limonada", "Té", "Jugos" }
+                {"Hamburguesa Sencilla", "Hamburguesa Especial", "Hamburguesa de la Casa"},
+                {"Perro Sencillo", "Perro Especial", "Super Dog"},
+                {"Perra Sencilla", "Perra Especial", "Super Doggy"},
+                {"Papas a la francesa"},
+                {"Queso", "Tocineta", "Ensalada"},
+                {"Gaseosa Personal", "Gaseosa Familiar", "Limonada", "Té", "Jugos"}
         };
 
         int[][] precios = {
-                { 14000, 18000, 21000 },
-                { 11000, 13000, 15000 },
-                { 12000, 14000, 16000 },
-                { 7000 },
-                { 5000, 5000, 5000 },
-                { 5000, 10000, 8000, 6000, 6000 }
+                {14000, 18000, 21000},
+                {11000, 13000, 15000},
+                {12000, 14000, 16000},
+                {7000},
+                {5000, 5000, 5000},
+                {5000, 10000, 8000, 6000, 6000}
         };
 
         int[][] cantidades = new int[6][];
@@ -81,135 +77,115 @@ public class ProyectoFinal {
         do {
             try {
                 tipoUsuario = Integer.parseInt(JOptionPane.showInputDialog("""
-                        Seleccione tipo de usuario:
-                        1. Administrador
-                        2. Cliente
-                        """));
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Ingrese una opción válida");
-                tipoUsuario = -1;
-            }
-        } while (tipoUsuario != 1 && tipoUsuario != 2);
+                Seleccione tipo de usuario:
+                1. Administrador
+                2. Cliente
+                """));
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Ingrese una opción válida");
+                    tipoUsuario = -1;
+                }
+            } while (tipoUsuario != 1 && tipoUsuario != 2);
 
         boolean esAdmin = false;
 
         if (tipoUsuario == 1) {
             String clave = JOptionPane.showInputDialog("Ingrese clave de administrador:");
-
-            if (clave.equals("1234")) {
+    
+            if (clave.equals("1234")) { // puedes cambiar la clave
                 esAdmin = true;
             } else {
                 JOptionPane.showMessageDialog(null, "Clave incorrecta. Entrando como cliente.");
             }
         }
-        //Commit 4
+
         int opcion;
 
-do {
+        do {
+            String menu = """
+                    --- MENU ---
+                    1. Hamburguesas
+                    2. Perros
+                    3. Perras
+                    4. Papas
+                    5. Adiciones
+                    6. Bebidas
+                    0. Finalizar
+                    """;
 
-    String menu = """
-            --- MENU ---
+            try {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+            } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Debes ingresar un número válido");
+            opcion = -1; // para que el ciclo continúe
+            continue;
+}
 
-            1. Hamburguesas
-            2. Perros
-            3. Perras
-            4. Papas
-            5. Adiciones
-            6. Bebidas
-            0. Finalizar
-            """;
+            switch (opcion) {
 
-    try {
+                case 1,2,3,4,5,6 -> {
 
-        opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+                int categoria = opcion - 1;
 
-    } catch (Exception e) {
-
-        JOptionPane.showMessageDialog(null, "Debes ingresar un número válido");
-        opcion = -1; // para que el ciclo continúe
-        continue;
-    }
-
-    switch (opcion) {
-
-        case 1, 2, 3, 4, 5, 6 -> {
-
-            int categoria = opcion - 1;
-
-            if (esAdmin == true) {
-
+                if (esAdmin == true) {
+                    
+                
                 int opcionAdmin;
 
-                do {
-
-                    try {
-
-                        opcionAdmin = Integer.parseInt(
-                                JOptionPane.showInputDialog("""
-                                ADMIN:
-                                1. Agregar producto
-                                2. Continuar
-                                """));
-
+            do {
+                try {
+                    opcionAdmin = Integer.parseInt(JOptionPane.showInputDialog("""
+                        ADMIN:
+                        1. Agregar producto
+                        2. Continuar
+                        """));
                     } catch (Exception e) {
-
                         JOptionPane.showMessageDialog(null, "Debes ingresar un número válido");
                         opcionAdmin = -1;
                         continue;
                     }
 
-                    if (opcionAdmin != 1 && opcionAdmin != 2) {
-
-                        JOptionPane.showMessageDialog(null, "Opción inválida");
-                    }
-
-                } while (opcionAdmin != 1 && opcionAdmin != 2);
-
-                if (opcionAdmin == 1) {
-
-                    String nuevo = JOptionPane.showInputDialog(
-                            "Nombre del nuevo producto:");
-
-                    int precioNuevo;
-
-                    try {
-
-                        precioNuevo = Integer.parseInt(
-                                JOptionPane.showInputDialog("Precio:"));
-
-                    } catch (Exception e) {
-
-                        JOptionPane.showMessageDialog(
-                                null,
-                                "Precio inválido"
-                        );
-
-                        break;
-                    }
-
-                    JOptionPane.showMessageDialog(
-                            null,
-                            "Producto agregado:\n" +
-                            nuevo + " - $" + precioNuevo
-                    );
+                if (opcionAdmin != 1 && opcionAdmin != 2) {
+                    JOptionPane.showMessageDialog(null, "Opción inválida");
                 }
+
+            } while (opcionAdmin != 1 && opcionAdmin != 2);
+
+            if (opcionAdmin == 1) {
+
+                String nuevo = JOptionPane.showInputDialog("Nombre del nuevo producto:");
+
+                int precioNuevo;
+            try {
+                precioNuevo = Integer.parseInt(JOptionPane.showInputDialog("Precio:"));
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Precio inválido");
+                continue; 
             }
+
+            String[] nuevosNombres = new String[nombres[categoria].length + 1];
+            int[] nuevosPrecios = new int[precios[categoria].length + 1];
+            int[] nuevasCantidades = new int[cantidades[categoria].length + 1];
+
+            for (int i = 0; i < nombres[categoria].length; i++) {
+                nuevosNombres[i] = nombres[categoria][i];
+                nuevosPrecios[i] = precios[categoria][i];
+                nuevasCantidades[i] = cantidades[categoria][i];
+            }
+
+            nuevosNombres[nombres[categoria].length] = nuevo;
+            nuevosPrecios[precios[categoria].length] = precioNuevo;
+            nuevasCantidades[cantidades[categoria].length] = 0;
+
+            nombres[categoria] = nuevosNombres;
+            precios[categoria] = nuevosPrecios;
+            cantidades[categoria] = nuevasCantidades;
+
+            JOptionPane.showMessageDialog(null, "Producto agregado correctamente");
         }
-
-        case 0 -> JOptionPane.showMessageDialog(
-                null,
-                "Pedido finalizado"
-        );
-
-        default -> JOptionPane.showMessageDialog(
-                null,
-                "Opción inválida"
-        );
     }
 
-} while (opcion != 0);
-        //Commit 5
-        String subMenu = "Seleccione:\n";
+                    String subMenu = "Seleccione:\n";
 
                     for (int i = 0; i < nombres[categoria].length; i++) {
                         subMenu += (i + 1) + ". " + nombres[categoria][i] + " $" + precios[categoria][i] + "\n";
@@ -259,7 +235,8 @@ do {
             }
 
         } while (opcion != 0);
-          // Commit 6:Mostrar factura final
+
+        // Mostrar factura final
         if (hayPedido(cantidades)) {
         String factura = generarFactura(nombres, precios, cantidades);
         JOptionPane.showMessageDialog(null, factura);
